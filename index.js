13c3ab628e9e59249df2f0834ce3f8ccfe5d682f
Nouhi <67205946+NouhiDev@users.noreply.github.com>
2023-01-17 20:05:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,6 @@ function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
-var typed = new Typed(".auto-type", {
-    strings: ["Developer", "Game Designer", "Graphic Designer"],
-    typeSpeed: 150,
-    backSpeed: 100,
-    startDelay: 3000,
-    loop: true
-});
-
 document.getElementById("btn").onclick = function () {
     location.href = "https://nouhidev.github.io";
 };
