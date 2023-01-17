2bd01fdf9ad55e7303e25308ae11915feb912ccd
Nouhi <67205946+NouhiDev@users.noreply.github.com>
2023-01-17 19:52:10 +0100

    a

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-$(window).on("load",async function(){ await delay(1000); $(".loader").fadeOut(1000); delay(1000).then(() => $(".content").fadeIn(1000)); }) 
+$(window).on("load",async function(){
+    await delay(2000);
+    $(".loader").fadeOut(1000);
+    delay(1000).then(() => $(".content").fadeIn(1000));
+})
 
-function delay(time) { return new Promise(resolve => setTimeout(resolve, time)); } 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+var typed = new Typed(".auto-type", {
+    strings: ["Developer", "Game Designer", "Graphic Designer"],
+    typeSpeed: 150,
+    backSpeed: 100,
+    startDelay: 3000,
+    loop: true
+});
+
+document.getElementById("btn").onclick = function () {
+    location.href = "https://nouhidev.github.io";
+};
