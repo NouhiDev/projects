7e530fc8f4c0c743b83286455e156bf2c63aeab1
Nouhi <67205946+NouhiDev@users.noreply.github.com>
2023-01-24 16:33:46 +0100

    Complete Overhaul

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-$(window).on("load",async function(){
-    // await delay(2000);
-    $(".loader").fadeOut(1000);
-    delay(1000).then(() => $(".content").fadeIn(1000));
-})
+// Loader
+$(window).on("load", async function () {
+  // await delay(2000);
+  $(".loader").fadeOut(1000);
+  await delay(1000).then(() => $(".content").fadeIn(1000));
+  document.getElementById("cards").style.display = "flex";
+});
 
 function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-function back_btn() {
-    location.href = "https://nouhidev.github.io";
-}
+// Hover Effect
+document.getElementById("cards").onmousemove = (e) => {
+  for (const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
 
-function school_projects_btn() {
-    location.href = "https://nouhidev.github.io/school-projects/";
-}
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+};
 
-function rblx_btn() {
-    location.href = "https://nouhidev.github.io/roblox-horrorlist/";
+// Card Onclicks
+function games_btn() {
+  location.href = "https://nouhi.dev/games/";
 }
 
 function gfx_btn() {
-    location.href = "https://www.behance.net/wolfgfx";
+  location.href = "https://www.behance.net/wolfgfx";
 }
 
-function itch_btn() {
-    location.href = "https://nouhidev.github.io/games/";
+function other_btn() {
+  location.href = "https://nouhi.dev/other-projects/";
+}
+
+function back_btn() {
+    location.href = "https://nouhi.dev/";
 }
